b4654ce3e23b392055d5a787907ed57d09b8109f
Gustavo Nozaki Soares <gnozakisoares@gmail.com>
2023-04-17 22:08:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        SortedDictionary<string, int> dict = new SortedDictionary<string, int>();
+
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(',');
+            string name = line[0];
+            int votes = int.Parse(line[1]);
+
+            if (dict.ContainsKey(name))
+            {
+                dict[name] += votes;
+            }
+            else
+            {
+                dict[name] = votes;
+            }
+        }
+
+        foreach (var item in dict)
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
